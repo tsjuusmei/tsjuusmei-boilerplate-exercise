9f45e4eff7c83b4413252455169f4ef636a1ab31
Nils Hoenson <nils@yummygum.com>
2020-04-28 10:38:23 +0200

    Additional helpers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 const withSass = require('@zeit/next-sass')
 
-module.exports = withSass()
+module.exports = withSass({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    })
+
+    return config
+  }
+})
