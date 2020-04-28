d277bab050820aeb501fb74e131384b115b800d6
Nils Hoenson <nils@yummygum.com>
2020-04-28 11:26:23 +0200

    Get started with new Grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,16 @@ import Button from '../components/atoms/Button'
 function Homepage() {
   return (
     <main className="home">
-      <div className="grid">
-        <div className="container-sm">
-          <h1 className="title-layer1-700">Next Boilerplate</h1>
-          <Button>Button</Button>
+      <div className="container-md">
+        <div className="grid">
+          <div className="col-6">
+            <h1 className="title-layer1-700">Next Boilerplate</h1>
+            <Button>Button</Button>
+          </div>
+          <div className="col-6">
+            <h1 className="title-layer1-700">Next Boilerplate</h1>
+            <Button>Button</Button>
+          </div>
         </div>
       </div>
     </main>
