3b90f5c041ddb4900102c19a7de17634777f16b7
donovanroubos <hello@donovanroubos.nl>
2020-06-24 15:35:38 +0200

    component: 🎛 Input
    Add Input component with default behavior
    
    ✅ Closes: #7

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Components
 import Button from '@/components/atoms/Button'
+import Input from '@/components/atoms/Input'
 
 function Homepage() {
   return (
@@ -10,7 +11,17 @@ function Homepage() {
         <div className="grid">
           <div className="col-6">
             <h2>Next Boilerplate</h2>
-            <Button>Button</Button>
+            <Button>Default</Button>
+            <Button href="https://yummygum.com">External link</Button>
+            <Button to="/somewhere">Internal route</Button>
+
+            <Input
+              name="input-name"
+              id="input"
+              type="text"
+              size="large"
+              placeholder="Text goes here"
+            />
           </div>
         </div>
       </div>
