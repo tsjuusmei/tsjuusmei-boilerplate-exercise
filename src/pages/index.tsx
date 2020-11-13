d352e1a21723e45567ab0fc71a7c4ac40fc8b56c
nils-yummygum <nils@yummygum.com>
2020-11-13 12:22:32 +0100

    component: Add Enum

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react'
 import { NextPage } from 'next'
 
-// Components
-import Button from '../components/atoms/Button'
-
 const Homepage: NextPage = () => (
   <main className="home">
     <div className="grid">
       <div className="container-sm">
         <h1 className="title-layer1-700">Next Boilerplate Typescript</h1>
-        <Button
-          variation='primary'
-          size='l'
-        >
-          Button
-        </Button>
       </div>
     </div>
   </main>
