6e92999e5798fc2ba4b44e24673d660f8b8cbb30
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2020-11-23 15:38:41 +0100

    styled button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import { NextPage } from 'next'
+import Button from '@/components/atoms/Button'
 
 const Homepage: NextPage = () => (
   <main className="home">
     <div className="grid">
       <div className="container-sm">
         <h1 className="title-layer1-700">Next Boilerplate Typescript</h1>
+        <Button variation="primary" size="md">
+          Button
+        </Button>
       </div>
     </div>
   </main>
