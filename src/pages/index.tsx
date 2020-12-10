75dbe0d5c5b20f396bb13245055d8f4f7d6fc1be
Idris Dopico Peña <idrisdopico@192.168.2.87>
2020-12-10 17:14:58 +0100

    Add correct function and add sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,12 @@ const Homepage: NextPage = () => (
   <main className="home">
     <div className="grid">
       <div className="container-sm">
-        <h1 className="title-layer1-700">Next Boilerplate Typescript</h1>
+        <h1>h1. Next Boilerplate Typescript</h1>
+        <h2>h2. Next Boilerplate Typescript</h2>
+        <h3>h3. Next Boilerplate Typescript</h3>
+        <h4>h4. Next Boilerplate Typescript</h4>
+        <h5>h5. Next Boilerplate Typescript</h5>
+        <h6>h6. Next Boilerplate Typescript</h6>
       </div>
     </div>
   </main>
