75fcbcd5d5209bbb79c6bfe1e92a1fb436d2d098
Idris Dopico Pena <dopico1996@hotmail.com>
2021-02-26 14:43:18 +0100

    Add storybook changes

--- src/components/atoms/Checkbox/Checkbox.stories.tsx
+++ src/components/atoms/Checkbox/Checkbox.stories.tsx
@@ -15,10 +15,10 @@ export default {
 }
 
 export const Primary = () => (
-  <>
+  <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
     {/* Default */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Default</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Default</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" />
@@ -27,8 +27,8 @@ export const Primary = () => (
     </div>
 
     {/* Default + Checked */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Default + Checked</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Default + Checked</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" isChecked={true} />
@@ -37,8 +37,8 @@ export const Primary = () => (
     </div>
 
     {/* Disabled */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Disabled</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Disabled</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" isDisabled={true} />
@@ -47,8 +47,8 @@ export const Primary = () => (
     </div>
 
     {/* Disabled + Checked */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Disabled + Checked</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Disabled + Checked</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" isDisabled={true} isChecked={true} />
@@ -57,8 +57,8 @@ export const Primary = () => (
     </div>
 
     {/* Error */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Error</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Error</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" hasError={true} />
@@ -67,8 +67,8 @@ export const Primary = () => (
     </div>
 
     {/* Error + Checked */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Error + Checked</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Error + Checked</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" hasError={true} isChecked={true} />
@@ -77,8 +77,8 @@ export const Primary = () => (
     </div>
 
     {/* Error + Disabled */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Error + Disabled</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Error + Disabled</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" hasError={true} isDisabled={true} />
@@ -87,14 +87,14 @@ export const Primary = () => (
     </div>
 
     {/* Error + Disabled + Checked */}
-    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px'}}>
-      <h3 style={{ marginBottom: '4px' }}>Error + Disabled + Checked</h3>
+    <div className="component-preview" style={{backgroundColor: '#f0f0f0', padding: '10px', maxWidth: '25%', flex: '25%'}}>
+      <h3 style={{ marginBottom: '8px' }}>Error + Disabled + Checked</h3>
       {Object.entries(sizes).map(([key], idx) => (
         <form style={{ margin: '0 0 20px' }}>
           <Checkbox key={idx} size={key as Sizes} label="Label" hasError={true} isDisabled={true} isChecked={true} />
         </form>
       ))}
     </div>
-  </>
+  </div>
 )
 
