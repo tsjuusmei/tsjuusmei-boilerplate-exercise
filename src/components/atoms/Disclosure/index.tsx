f05f1921cf56f0ed77f1b37ab566fed5a61e94b3
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2021-04-21 12:33:16 +0200

    component: Add useState to Disclosure component
    Add useState to Disclosure componenet to prevent using important in css
    to animate chevron element
    
    Closes: CHL-36

--- src/components/atoms/Disclosure/index.tsx
+++ src/components/atoms/Disclosure/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Components
 import Icon from '@/components/atoms/Icon'
@@ -15,23 +15,39 @@ export type DisclosureProps = {
 const Disclosure: React.FC<DisclosureProps> = ({
   summary,
   body,
-  hasIcon = false,
-}) => (
-  <details className={styles.details}>
-    <summary>
-      <h5>
-        {hasIcon &&
-          <>
-            <Icon name="minus" size={24} />
-            <Icon name="plus" size={24} />
-          </>
-        }
-        {summary}
-        <Icon name="chevron" size={24} />
-      </h5>
-    </summary>
-    <p className={`${hasIcon ? 'has-icon' : ''}`}>{body}</p>
-  </details>
-)
+  hasIcon = false
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleIsOpen(e: { preventDefault: () => void }) {
+    e.preventDefault()
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <details
+      className={styles.details}
+      open={isOpen}
+      onClick={toggleIsOpen}
+    >
+      <summary>
+        <h5>
+          {hasIcon &&
+            <>
+              <Icon name="minus" size={24} />
+              <Icon name="plus" size={24} />
+            </>
+          }
+          {summary}
+          <Icon
+            name="chevron"
+            direction={isOpen ? 'down' : 'up'}
+            size={24}
+          />
+        </h5>
+      </summary>
+      <p className={`${hasIcon ? 'has-icon' : ''}`}>{body}</p>
+    </details>
+  )
+}
 
 export default Disclosure
