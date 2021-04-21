503721b3579c9e1a68b7924ae1c63856132d0cbf
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2021-04-21 11:15:46 +0200

    bug: Fixing TextArea storybook error
    Fixing TextArea storybook error

---
+++ src/components/atoms/TextArea/index.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+
+//Components
+import Icon from '@/components/atoms/Icon'
+
+// Styles
+import styles from  './TextArea.module.scss'
+
+type Props = {
+  id?: string,
+  label?: string,
+  isError?: boolean,
+  size?: Sizes,
+  placeholder?: string,
+  spellCheck?: boolean,
+  autoComplete?: string,
+  isDisabled?: boolean,
+  name: string,
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void,
+  onFocus?: (e: React.FocusEvent<HTMLTextAreaElement>) => void,
+  onBlur?: (e: React.FormEvent<HTMLTextAreaElement>) => void,
+  isOptional?: boolean
+}
+
+const Textarea: React.FC<Props> = ({
+  id,
+  label,
+  size = 'md',
+  placeholder,
+  isDisabled,
+  isError,
+  isOptional,
+  onChange,
+  onFocus,
+  onBlur,
+  autoComplete = 'on',
+  spellCheck = true
+}) => {
+  return (
+    <div className={styles.textarea}>
+      {label && (
+        <label htmlFor={id} className={styles.label}>
+          {label}
+          {isError && (
+            <Icon name="warning" color="var(--error-500)"/>
+          )}
+          {isOptional && <span>Optional</span>}
+        </label>
+      )}
+
+      <textarea
+        className={`
+          ${styles.element}
+          ${styles[size]}
+          ${isError ? styles.error : null}
+        `}
+        onChange={onChange}
+        id={id}
+        placeholder={placeholder}
+        disabled={isDisabled}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        autoComplete={autoComplete}
+        spellCheck={spellCheck}
+      />
+    </div>
+  )
+}
+
+export default Textarea
