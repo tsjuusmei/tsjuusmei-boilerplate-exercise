6b2ec8653c7e4dd295e400b415a3364c5b13f73a
goemaereshannon <shannon.goemaere@student.howest.be>
2021-05-19 16:26:34 +0200

    CHL-40: Refactor button component

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -8,8 +8,7 @@ import styles from './Button.module.scss'
 export enum ButtonVariation {
   Primary = 'primary',
   Secondary = 'secondary',
-  Tertiary = 'tertiary',
-  TextLink = 'text-link'
+  Tertiary = 'tertiary'
 }
 
 export enum ButtonSize {
@@ -28,7 +27,6 @@ export type ButtonProps = {
   variation?: ButtonVariation,
   size?: ButtonSize,
   label?: string,
-  href?: string,
   isFullWidth?: boolean,
   isDisabled?: boolean,
   onClick?: React.MouseEventHandler<HTMLElement>,
@@ -43,28 +41,20 @@ const Button: React.FC<ButtonProps> = ({
   type = ButtonType.Button,
   children,
   label = '',
-  href = '',
   isFullWidth = false,
   isDisabled = false,
   className = '',
   contentClassName = '',
   ...props
 }) => {
-  // Check if link is internal or external
-  const isInternalLink = href && !href.includes('http')
-
-  // When link is internal, Link > a > div
-  const LinkOrButton = href ? 'a' : 'button'
-
   // Shared classNames (less duplicate code)
   const sharedClassNames = `${styles[size]} ${styles[variation]} ${isFullWidth ? styles.fullwidth : ''} ${isDisabled ? styles.disabled : ''}`
 
   // TODO: Need to split off the 'text link' button(s) from the main component.
   // This will also make it easier to change the 'any' type to 'HTMLButtonElement'.
-  const ButtonWrapper = React.forwardRef<any, ButtonProps>((_, ref) => (
-    <LinkOrButton
+  return (
+    <button
       {...props}
-      ref={ref}
       className={`
         ${styles.button}
         ${className}
@@ -73,12 +63,7 @@ const Button: React.FC<ButtonProps> = ({
       disabled={isDisabled}
       aria-disabled={isDisabled}
       aria-label={label}
-      href={href}
-      type={href ? null : type}
-      {...(!isInternalLink && {
-        rel: 'noopener noreferrer',
-        target: '_blank'
-      })}
+      type={type}
     >
       <div
         className={`
@@ -89,20 +74,8 @@ const Button: React.FC<ButtonProps> = ({
       >
         {children}
       </div>
-    </LinkOrButton>
-  ))
-
-  ButtonWrapper.displayName = 'ButtonWrapper'
-
-  if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <ButtonWrapper variation={variation} size={size} />
-      </Link>
-    )
-  }
-
-  return (<ButtonWrapper variation={variation} size={size} {...props} />)
+    </button>
+  )
 }
 
 export default Button
